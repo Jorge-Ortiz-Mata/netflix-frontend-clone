b4cdd5ec6e6d8ca73c37ed41e2addeacd2b57b3a
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-04-25 18:26:39 -0500

    Adding Navbar configuration and logo.

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,10 +1,12 @@
+import NetflixLogo from '../../assets/Netflix.png';
+
 const Navbar = () => {
 
   return(
     <header className="flex items-center w-full">
-      <nav className="flex items-center w-full justify-between py-7 px-10">
+      <nav className="flex items-center w-full justify-between py-6 px-12">
         <div>
-          Image
+          <img src={NetflixLogo} alt="netflix-log" className='h-10' />
         </div>
 
         <div>
