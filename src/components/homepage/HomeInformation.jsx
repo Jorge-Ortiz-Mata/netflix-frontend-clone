91817d2edd71896d4f48ebd07544cf01fbe1697f
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-04-25 20:08:06 -0500

    Styling HomePage.

--- src/components/homepage/HomeInformation.jsx
+++ src/components/homepage/HomeInformation.jsx
@@ -1,10 +1,10 @@
 const HomeInformation = () => {
 
   return(
-    <div>
-      <h2>Unlimited movies, TV shows, and more.</h2>
-      <p>Watch anywhere. Cancel anytime.</p>
-      <p>Ready to watch? Enter your email to create or restart your membership.</p>
+    <div className="w-full flex flex-col h-[500px] m-20 py-10 gap-5">
+      <h2 className="text-6xl font-black">Unlimited movies, TV shows, and <br /> more.</h2>
+      <p className="font-semibold text-2xl my-10">Watch anywhere. Cancel anytime.</p>
+      <p className="text-xl">Ready to watch? Enter your email to create or restart your membership.</p>
     </div>
   )
 }
